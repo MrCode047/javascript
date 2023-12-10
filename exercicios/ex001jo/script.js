46cc2b5514e7147d81544b19246fb555c08203d5
Jory Aguet <142528473+MrCode047@users.noreply.github.com>
2023-12-10 23:24:50 +0000

    -

--- exercicios/ex001jo/script.js
+++ exercicios/ex001jo/script.js
@@ -3,7 +3,15 @@ let msg = document.getElementById("msg")
 let img = document.getElementById("img")
 let data = new Date()
 let heure = data.getHours()
-msg.innerHTML = `Il est ${heure} heures !`
+msg.innerHTML = `<strong>Il est ${heure} heures !</strong>`
 
-
+    if (heure >= 0 && heure <=12) {
+    img.src = "imagens/matin.jpg"
+    document.body.style.background = "darkgrey"
+    } else if (heure >=13 && heure <= 20){
+    img.src = "imagens/jour.jpg"
+    document.body.style.background = "chocolate"
+    } else {
+    img.src = "imagens/nuit.jpg"
+    }
 }
