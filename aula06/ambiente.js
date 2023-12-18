a9a639b2883ae0440396f349673d3a42be3c5589
Jory Aguet <142528473+MrCode047@users.noreply.github.com>
2023-12-18 17:03:35 +0000

    -

---
+++ aula06/ambiente.js
@@ -0,0 +1,16 @@
+let contador = 1
+do {
+    console.log(`Passo ${contador}`)
+    contador++ // contador = contador + 1
+} while (contador <= 100)
+
+
+
+/* 
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?") 
+*/
---
+++ aula06/node_953c5ed00b5e8.tmp.js
@@ -0,0 +1,16 @@
+let contador = 1
+while (contador <= 100) {
+    console.log("Ola,mundo!")
+    
+}
+
+
+
+/* 
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?") 
+*/
