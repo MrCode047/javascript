702538a0082c291ef95f29b228222bad22e17e80
Jory Aguet <142528473+MrCode047@users.noreply.github.com>
2023-12-18 17:46:51 +0000

    -

--- aula06/ambiente.js
+++ aula06/ambiente.js
@@ -1,14 +1,19 @@
-let contador = 1
+/* let contador = 1
 
 while (contador <= 100) {
     console.log(`Passo ${contador}`)
     contador++
 }
 
-/* 
 ou
+
 do {
     console.log(`Passo ${contador}`)
-    contador++ // contador = contador + 1
+    contador++ 
 } while (contador <= 100) 
-*/
+
+ou */
+
+for (let contador = 1; contador<=100; contador++) {
+    console.log(`Passo ${contador}`)
+}
