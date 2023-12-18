733ce412af6b165a4e15d4bc916d31cb05c322e1
Jory Aguet <142528473+MrCode047@users.noreply.github.com>
2023-12-18 18:52:50 +0000

    -

---
+++ aula06/exercicios/ex003/node_be4b42aee56a5.tmp.js
@@ -0,0 +1,15 @@
+function contar() {
+    let inicio = document.getElementById("inum")
+    let fim = document.getElementById("ifim")
+    let passo = document.getElementById("ipass")
+    let res = document.querySelector("div#res")
+    
+}
+inicio = 1
+fim = 2
+passo = 0
+ 
+    while (inicio == 1) {
+        passo++
+        console.log(passo)
+    }
