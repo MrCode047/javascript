36b14723a10f4f4e9d3c7896de4358e485035329
Jory Aguet <142528473+MrCode047@users.noreply.github.com>
2023-12-19 12:26:53 +0000

    -

--- aula06/exercicios/ex003/script.js
+++ aula06/exercicios/ex003/script.js
@@ -1,19 +1,36 @@
 function contar() {
-    let inicio = document.getElementById("inum")
-    let fim = document.getElementById("ifim")
-    let passo = document.getElementById("ipass")
-    let res = document.querySelector("div#res")
-      
-}
-  for(inicio == inicio;fim == fim;passo++) {
-        res.innerHTML = `Contando ${res}`
+  let inicio = document.getElementById("txti");
+  let fim = document.getElementById("txtf");
+  let passo = document.getElementById("txtp");
+  let res = document.querySelector("div#res");
+
+  if (
+    inicio.value.length == 0 ||
+    fim.value.length == 0 ||
+    passo.value.length == 0
+  ) {
+    res.innerHTML = "Impossivel contar !"
+    alert("[ERRO] Faltam dados !");
+  } else {
+    res.innerHTML = "Contando: <br>";
+    let i = Number(inicio.value);
+    let f = Number(fim.value);
+    let p = Number(passo.value);
+    if (p<=0) {
+        alert("Passo invalido. Considerando PASSO 1")
+        p = 1
+    }
+    if (i < f) {
+      // Contagem crescente
+      for (let c = i; c <= f; c += p) {
+        res.innerHTML += ` ${c} \u{1F449}`;
       }
-
-
-/* inicio = 1
-passo = 0
- 
-    while (inicio == 1) {
-        passo++
-        console.log(`Passo ${passo}`)
-    } */
+    } else {
+      // Contagem regressiva
+      for (let c = i; c >= f; c -= p) {
+        res.innerHTML += ` ${c} \u{1F449}`;
+      }
+    }
+    res.innerHTML += ` \u{1F3C1}`;
+  }
+}
